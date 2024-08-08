61de589e5a5684e20b6cac00dd93d0d6bded24f7
aizen <tarunsanjayrao@gmail.com>
2024-08-09 01:31:25 +0530

    wallet/navbar setup

---
+++ app/components/AppBar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { FC } from "react";
+// import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+import styles from "./AppBar.module.css";
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+export const AppBar: FC = () => {
+  return (
+    <div className="h-[120px] flex bg-coal flex-row items-center justify-between text-[50px] text-white px-[20px] flex-wrap">
+      <Image src="/solanaLogo.png" alt="solana" height={30} width={200} />
+
+      <span className={styles.gradient}>Solana Token Mint</span>
+      <WalletMultiButtonDynamic />
+    </div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Orbitron } from "next/font/google";
 import "./globals.css";
+import { WalletContextProvider } from "./components/WalletContextProvider";
+import { AppBar } from "./components/AppBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const orbitron = Orbitron({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={orbitron.className}>
+        <WalletContextProvider>
+          <AppBar />
+          {children}
+        </WalletContextProvider>
+      </body>
     </html>
   );
 }
